worked on the normalisation and stats a bit. Inverted the call to scipy.stats.ttest do the sign of the T statistic is correct

normalise.py
@@ -8,7 +8,7 @@ import harwellimglib as hil
 
 
 
-def normalise(indir, outdir, size, index):
+def normalise(indir, outdir, z, y, x):
     print 'normming', indir
     outdir = os.path.abspath(outdir)
     indir = os.path.abspath(indir)
@@ -19,10 +19,10 @@ def normalise(indir, outdir, size, index):
         except Exception as e:
             print "cant read {}: {}".format(path, e)
 
-        roi = sitk.RegionOfInterest(im, size, index)
-        arr = sitk.GetArrayFromImage(roi)
+        arr = sitk.GetArrayFromImage(im)
+        roi = arr[z[0]: z[1], y[0]: y[1], x[0]: x[1]]
 
-        mean_ = float(np.mean(arr))
+        mean_ = float(np.mean(roi))
 
         # Normalise
         norm = sitk.IntensityWindowing(im, mean_)
@@ -36,7 +36,8 @@ if __name__ == '__main__':
     in_ = sys.argv[1]
     out = sys.argv[2]
 
-    s = [9, 7, 8]
-    i = [103, 131, 323]
+    z = (325, 331)
+    y = (133, 139)
+    x = (104,110)
 
-    normalise(in_, out, s, i)
+    normalise(in_, out, z, y, x)

reg_stats.py
@@ -105,7 +105,6 @@ def get_vector_magnitudes(img):
     print "getting deformation magnitudes"
     arr = sitk.GetArrayFromImage(img)
     #Get the mean vector. Then get the magnitude of it using np.linalg.norm
-    print "vectors shapea", arr.shape, type(arr), arr.dtype
     scalars = np.sqrt((arr*arr).sum(axis=3))
     return sitk.GetImageFromArray(scalars)
 
@@ -129,7 +128,7 @@ def filter_tsats(tstats, qvalues):
     assert len(tstats) == len(qvalues)
     t = np.array(tstats)
     q = np.array(qvalues)
-    mask = q > 0.1
+    mask = q > 0.07
     t[mask] = 0
 
     return t
@@ -149,7 +148,7 @@ def ttest(wt, mut):
     wt_flat = flatten(wt)
     mut_flat = flatten(mut)
 
-    tscores, pvals = stats.ttest_ind(wt_flat, mut_flat)
+    tscores, pvals = stats.ttest_ind(mut_flat, wt_flat)
 
     mask_t = np.isnan(tscores)
     tscores[mask_t] = 0
@@ -174,7 +173,6 @@ def memory_map_volumes(vol_paths, memmap=False, analysis_type='int'):
 
     vols = []
     for vp in vol_paths:
-        print 'yyyy', vp
         img = sitk.ReadImage(vp)
         blurred = blur(img, analysis_type)
         array = sitk.GetArrayFromImage(blurred)
@@ -231,4 +229,4 @@ if __name__ == '__main__':
 
 
 
-    stats(args.wt_vols_dir, args.mut_vols_dir, args.analysis_type, args.outfile, args.mask, args.memmap)
+    reg_stats(args.wt_vols_dir, args.mut_vols_dir, args.analysis_type, args.outfile, args.mask, args.memmap)