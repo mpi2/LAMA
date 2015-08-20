53258d12e70e054a018a4a61f7ab782e1a7c23b0
n.horner <n.horner@har.mrc.ac.uk>
2015-08-20 10:40:10 +0100

    Integrated the mesh inversion. Still a bit buggy
    Former-commit-id: 04646836a632479aa714c96411c978aa7b1657ef [formerly 163e7db04cd24163fb8ad4d5292211614e932e0c]
    Former-commit-id: 96ed8df4abb35d7ab75f3ea3258e8c16ce3665d4

--- isosurfaces.py
+++ isosurfaces.py
@@ -1,41 +1,104 @@
-__author__ = 'james'
+#!/usr/bin/env python
+
 
 import argparse
 import SimpleITK as sitk
 from os.path import join, dirname
 import vtk
+import csv
+
+import common
+from invert import BatchInvertMeshes
 
 
-def generate_isosurfaces(input_nrrd, out_dir, organ_names):
+def generate_isosurfaces(inversion_config, input_nrrd, out_dir, organs_csv):
+    """
+    Cannot take compressed nrrds at the moment as VTK does not support them
+
+    Parameters
+    ----------
+    inversion_config: str
+        path to yaml file with directory names containing the elastix inversion parameter files
+        eg:
+    """
 
     # Create smoothed labelmap
     im = sitk.ReadImage(input_nrrd)
-    labelmap = join(dirname(input_nrrd), 'seg_smoothed.nrrd')
+    labelmap = join(dirname(input_nrrd), 'seg_smoothed.tiff')
     smooth_and_fill_label_map(im, labelmap)
 
-    # Create NRRD reader
-    reader = vtk.vtkNrrdReader()
+    organ_names = []
+    with open(organs_csv, 'r') as on:
+            reader = csv.reader(on)
+            for row in reader:
+                try:
+                    organ_names.append(row[0])
+                except IndexError:
+                    pass
+
+    reader = vtk.vtkTIFFReader()
     reader.SetFileName(labelmap)
 
+    mesh_dir = join(out_dir, 'meshes')
+    common.mkdir_if_not_exists(mesh_dir)
+
+    elastix_points_dir = join(out_dir, 'inverted_meshes_elx_points')
+    common.mkdir_if_not_exists(elastix_points_dir)
+
+    mesh_filenames = []
     for i, organ in enumerate(organ_names):
 
         file_name = '{}.vtk'.format(organ)
-        vtk_out = join(out_dir, file_name)
+        mesh_filenames.append(file_name)
+        vtk_out = join(mesh_dir, file_name)
         make_mesh(i+1, reader, vtk_out)
+        BatchInvertMeshes(inversion_config, vtk_out, elastix_points_dir)
+
+
+# def vtk_to_elastix_points(vtk_file, outfile):
+#     """
+#     Converts vtk points from a polygon file to a format that easltix can use
+#     """
+#
+#     def points(l, n):
+#         """
+#         Yield successive n-sized chunks from l.
+#         """
+#         for i in xrange(0, len(l), n):
+#             yield l[i:i+n]
+#
+#     point_lines = []
+#     numpoints = 0
+#
+#     with open(vtk_file, 'r') as fin, open(outfile, 'w') as fout:
+#         start_reading = False
+#         for line in fin.readlines():
+#             if start_reading:
+#                 l = line.split()
+#                 if line.startswith('POLYGONS'):
+#                     break
+#                 for p in points(l, 3):
+#                     numpoints += 1
+#                     point_lines.append(" ".join(p) + "\n")
+#             elif line.startswith('POINTS'):
+#                 start_reading = True
+#
+#         fout.write("{}\n".format(numpoints))
+#         fout.writelines(point_lines)
 
 
 def smooth_and_fill_label_map(im, out_path):
 
     # Morphological opening
-    opening = sitk.BinaryMorphologicalOpeningImageFilter()
-    opening.SetKernelRadius([2, 2, 2])
-    opening.SetKernelType(sitk.sitkBall)
-    opened = opening.Execute(im > 0)
+    # opening = sitk.BinaryMorphologicalOpeningImageFilter()
+    # opening.SetKernelRadius([1, 1, 1])
+    # opening.SetKernelType(sitk.sitkBall)
+    # opened = opening.Execute(im > 0)
 
     # Fill holes
     filler = sitk.BinaryFillholeImageFilter()
     filler.FullyConnectedOn()
-    filled = filler.Execute(opened)
+    filled = filler.Execute(im)
 
     # Merge the mask with the original image
     masking = sitk.MaskImageFilter()
@@ -89,11 +152,15 @@ def save_vtk(vtk_data, output_path):
     vtk_writer.Write()
 
 if __name__ == '__main__':
-
+    import sys
     parser = argparse.ArgumentParser()
-    parser.add_argument('-i', '--input', dest='vol', required=True, help="Input labelmap e.g. nrrd")
-    parser.add_argument('-o', '--output_dir', dest='out_dir', required=True, help="Output directory")
-    parser.add_argument('-n', '--names', dest='organ_names', required=True, help="CSV file of organ names")
-    args = parser.parse_args()
+    parser.add_argument('-c', '--config', dest='config', required=True, help="Input labelmap e.g. nrrd")
+    parser.add_argument('-i', '--invertable', dest='vol', required=True, help="Output directory")
+    parser.add_argument('-o', '--out', dest='out_dir', required=True, help="CSV file of organ names")
+    parser.add_argument('-n', '--names', dest='organ_names', required=True, help="yaml cofig with inversion order")
 
-    generate_isosurfaces(args.vol, args.out_dir, args.organ_names)
+    args, _ = parser.parse_known_args()
+
+
+
+    generate_isosurfaces(args.config, args.vol, args.out_dir, args.organ_names)
