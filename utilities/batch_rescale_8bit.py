f53bf5cd24a5dc0460796dfbc4dd380f761beb24
Neil Horner <n.horner@har.mrc.ac.uk>
2015-11-02 16:42:11 +0000

    Messing around with pvalues
    Former-commit-id: f65108abdbd07a34132a23c8a6d1772707ec7ab2 [formerly 712ed540001a97a35d11d6daae1d1066286847be]
    Former-commit-id: af8ea000d606e79538bb6671ff579b71666fd9e9

---
+++ utilities/batch_rescale_8bit.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+import SimpleITK as sitk
+import os
+import sys
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..'))
+import common
+
+
+if len(sys.argv) > 2:
+    indir = sys.argv[1]
+    outdir = sys.argv[2]
+else:
+    indir = sys.argv[1]
+    # Relplace volumes
+    outdir = sys.argv[1]
+
+
+paths = common.GetFilePaths(indir)
+
+for path in paths:
+    img = sitk.ReadImage(path)
+    cast = sitk.Cast(sitk.RescaleIntensity(img), sitk.sitkUInt8)
+    basename = os.path.basename(path)
+    outpath = os.path.join(outdir, basename)
+    sitk.WriteImage(cast, outpath)
+
+
