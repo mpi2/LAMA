510524905344b5b2b7db65c4d2b8888bd9d31cc0
n.horner <n.horner@har.mrc.ac.uk>
2016-01-21 19:55:15 +0000

    ?
    Former-commit-id: a7c30cb0955cb233486d07eaaee0eaab18fd4714

---
+++ organ_shrinking/hill_climer_itk/HillClimbITK_test/CMakeFiles/HillClimbITK.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/HillClimbITK.dir/HillClimbITK.cxx.o"
+  "HillClimbITK.pdb"
+  "HillClimbITK"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/HillClimbITK.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
