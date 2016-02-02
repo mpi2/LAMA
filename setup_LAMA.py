71bf2664226de8566cf348d21e08bc27fb0c0259
Neil Horner <n.horner@har.mrc.ac.uk>
2016-02-02 15:40:19 +0000

    logging fixed
    Former-commit-id: a3ccfc14080a24060fc42d505d081b9373ea047a [formerly 0dc8cb45c580097b6580ccb5d531bc0f27b7c93e]
    Former-commit-id: f14a031a1b4cddf65bc3c5e8667e629a84109949

--- setup_LAMA.py
+++ setup_LAMA.py
@@ -6,22 +6,28 @@ print "=== LAMA phenotyping pipeline ===\nDownloading dependencies..."
 # easy_install first
 try:
     from setuptools.command import easy_install
-    dependencies = ["numpy", "scipy" "pyyaml", "SimpleITK"]
-    dependencies_named = {
-        "yaml": "pyyaml",
+    dependencies = ["numpy", "scipy", "SimpleITK", 'appdirs', 'psutil']
+    dependencies = {
+        'yaml': 'pyyaml',
+        'scipy': 'scipy',
+        'numpy': 'numpy',
+        'SimpleITK': 'SimpleITK',
+        'appdirs': 'appdirs',
+        'psutil': 'psutil',
+        'yaml': 'pyyaml'
 
     }
 
-    for dep in dependencies:
+    for import_name, package_name in dependencies.iteritems():
 
         try:
-            print "Installing {0}...".format(dep),
-            mod = __import__(dep)  # try to import module
-            print " already installed.".format(dep)
+            print "Installing {0}...".format(import_name),
+            mod = __import__(import_name)  # try to import module
+            print " already installed.".format(import_name)
 
         except ImportError:
             # If it fails, try to easy install it
-            easy_install.main(["--user", dep])
+            easy_install.main(["--user", package_name])
             print "done."
 
 except ImportError:
