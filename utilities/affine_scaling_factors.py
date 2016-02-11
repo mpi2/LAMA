bf7e18dab1188bcd6fa063a6a16317b0574bdbba
Neil Horner <n.horner@har.mrc.ac.uk>
2016-02-11 18:19:09 +0000

    Fixed wt intensity dir not being found when normalization occures. Added script to determine scaling factor in affine stage
    Former-commit-id: fee58d914112c21fe2eea7b4766f883e510012a3 [formerly 6603fe9c387f9999c40ad9b54b35df25a07927a1]
    Former-commit-id: dad77217a446966be51083d04644e90218c4e815

---
+++ utilities/affine_scaling_factors.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+
+import transformations as trans
+import numpy as np
+import os
+
+
+TFORM_FILE_NAME = 'TransformParameters.0.txt'
+AFFINE_INDENTIFIER = '(Transform "AffineTransform")'
+TFORM_PARM_LINE_START = '(TransformParameters'
+
+def get_scaling_factor(tform_params):
+
+    mat = np.array(tform_params).reshape(4, 3).T
+    hom = np.array([0, 0, 0, 1])
+    hom_mat = np.vstack((mat, hom))
+    s = trans.decompose_matrix(hom_mat)
+    scaling = s[0][:3]
+
+    return scaling[2]
+
+
+def extract_affine_transformation_parameters(path):
+    for file_ in os.listdir(path):
+        if not file_ == TFORM_FILE_NAME:
+            continue
+        with open(os.path.join(path, file_)) as reader:
+            first_line = reader.next()
+            if not first_line.startswith(AFFINE_INDENTIFIER):
+                print 'This Transform parameter file does not look like an affine transform'
+            for line in reader:
+                if line.startswith(TFORM_PARM_LINE_START):
+                    tform_params = line.strip().strip(')').split()[1:]
+                    return [float(x) for x in tform_params]
+
+
+if __name__ == '__main__':
+    import sys
+    path = sys.argv[1]
+    tform_parms = extract_affine_transformation_parameters(path)
+    get_scaling_factor(tform_parms)
