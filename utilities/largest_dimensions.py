cc386ac27bff869ad04067509ec7ecb8eb41097e
Neil Horner <n.horner@har.mrc.ac.uk>
2016-02-15 09:30:48 +0000

    MAde affine scaling factor script batchable
    Former-commit-id: 645cdf8ae581d49cc668f491ac5bc676a0c4d34e [formerly 56a4082764df3ec3edfd7f606b86e1c9d18bbaa5]
    Former-commit-id: f41c964b2681aa777a1b61b58f4416e676de49c2

---
+++ utilities/largest_dimensions.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+import sys
+from os.path import join, dirname
+sys.path.insert(0, join(dirname(__file__), '..'))
+import common
+import numpy as np
+
+def run(folder):
+    max_zyx = [0, 0, 0]
+    for file_ in common.GetFilePaths(folder):
+        shape = common.img_path_to_array(file_).shape
+        max_zyx = np.amax((max_zyx, shape), axis=0)
+
+    print max_zyx
+
+
+if __name__ == '__main__':
+
+    folder = sys.argv[1]
+    run(folder)
