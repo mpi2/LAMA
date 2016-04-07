57215725221bac3daa6ff4b9d548adb2ad0f2a09
n.horner <n.horner@har.mrc.ac.uk>
2016-04-07 17:30:26 +0100

    Angles
    Former-commit-id: 6c0e5de43d7b349ff177591dc503d8ebacef9623 [formerly e2cf169803c34e25c85138493ebd2c39cc75b17b]
    Former-commit-id: 42049e04d82871a6cb92167de61f263614956286

--- stats/circular.R
+++ stats/circular.R
@@ -1,12 +1,19 @@
 library(circular)
+library(compiler)
 
 args <- commandArgs(trailingOnly = TRUE);
 
-pixels_file <- '/home/neil/work/circular_stats/test_cir.dat';
-groups_file <- '/home/neil/work/circular_stats/groups.csv';
-pvals_out <- '/home/neil/work/circular_stats/pvals';
-tvals_out <- '/home/neil/work/circular_stats/tvals';
-formula <- 'genotype';
+pixels_file <- args[1];
+groups_file <- args[2];
+pvals_out <- args[3];
+tvals_out <- args[4];
+formula <- args[5]; # Just foing one formula at a time for now
+
+# pixels_file <- '/home/neil/work/circular_stats/test_cir.dat';
+# groups_file <- '/home/neil/work/circular_stats/groups.csv';
+# pvals_out <- '/home/neil/work/circular_stats/pvals';
+# tvals_out <- '/home/neil/work/circular_stats/tvals';
+# formula <- 'genotype';
 
 
 # Create a data frame of the groups
@@ -31,21 +38,22 @@ f = factor(groups$genotype)
 
 wwtest <- function(data){
   ww = watson.wheeler.test.default(data, group = f);
-  return(pval=ww$p.value)
+  c(ww$p.value, ww$statistic)
 }
 
+wwtest_comp <- cmpfun(wwtest)
 
-result <- apply(mat, 2, wwtest)
+result <- apply(mat, 2, wwtest_comp)
 #results <- p,andt_vals(fit)
 
 
 
 poutCon <- file(pvals_out, "wb")
-writeBin(unlist(result), poutCon);
+writeBin(unlist(result[1,]), poutCon);
 close(poutCon);
 
-#toutCon <- file(tvals_out, "wb");
-#writeBin(result$tval, toutCon);
-#close(toutCon)
+toutCon <- file(tvals_out, "wb");
+writeBin(unlist(result[2,]), toutCon);
+close(toutCon)
 
 
