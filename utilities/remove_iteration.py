57760d464ecd48146e1bd0a9157c2e545ddbfc29
n.horner <n.horner@har.mrc.ac.uk>
2016-05-12 17:43:53 +0100

    utility script
    Former-commit-id: daf918204999007e1b4c1934e2e68ee5af055389 [formerly 5e588bf35059c578dab2a1983e967da8c588e857]
    Former-commit-id: 0cdf8aa1588fb372c25a9c98c383ce02d5545e82

---
+++ utilities/remove_iteration.py
@@ -0,0 +1,22 @@
+import sys
+import os
+
+
+dir_ = sys.argv[1]
+
+for subdir, dirs, files in os.walk(dir_):
+    for file in files:
+        #print os.path.join(subdir, file)
+        filepath = subdir + os.sep + file
+
+        if filepath.endswith(".asm"):
+            lines = []
+            with open(filepath, 'rb') as fh:
+                for line in fh:
+                    if line.startswith('InitialTransformParametersFileName'):
+                        continue
+                line.append(line)
+            with open(filepath, 'wb') as wh:
+                for line in lines:
+                    wh.write(line)
+
