a16eadcb2fdbdc80ad0a28c2b28a51f57aac2295
Neil horner <n.horner@har.mrc.ac.uk>
2016-05-16 13:01:39 +0100

    ?
    Former-commit-id: ed83b4bb31726678aa3c215623d90b6cbba65a27 [formerly 2c428df04d393c39fc0218dded881e3aa271fdf6]
    Former-commit-id: a6e787d4c252122d514e53e4a50bd78d5df3ffa4

--- batch_invert_meshes.py
+++
@@ -1,9 +0,0 @@
-import sys
-
-from mrch_regpipeline import invert_isosurfaces as iso
-
-invert_config = sys.argv[1]
-mesh_dir = sys.argv[2]
-iso_out_dir = sys.argv[3]
-
-iso(invert_config, mesh_dir, iso_out_dir)
