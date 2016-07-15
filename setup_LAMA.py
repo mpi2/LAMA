97793672015e82fb0ca688cd986438191dd8af0e
Neil horner <n.horner@har.mrc.ac.uk>
2016-07-15 10:31:36 +0100

    Updated the lama setup script
    Former-commit-id: 2689394d630fbb75aad5201c76daf79b1a6a0f65 [formerly 9ce201fbb0ed6fc5eb59c0c8587cefa22f277eba]
    Former-commit-id: 0654fb7fe59d50e8b5aa67744d568d1a91067a34

--- setup_LAMA.py
+++ setup_LAMA.py
@@ -5,30 +5,49 @@ print "=== LAMA phenotyping pipeline ===\nDownloading dependencies..."
 
 # easy_install first
 try:
-    from setuptools.command import easy_install
-    dependencies = {
-        'yaml': 'pyyaml',
-        'scipy': 'scipy',
-        'numpy': 'numpy',
-        'SimpleITK': 'SimpleITK',
-        'appdirs': 'appdirs',
-        'psutil': 'psutil',
-        'yaml': 'pyyaml'
-
-    }
-
-    for import_name, package_name in dependencies.iteritems():
-
-        try:
-            print "Installing {0}...".format(import_name),
-            mod = __import__(import_name)  # try to import module
-            print " already installed.".format(import_name)
-
-        except ImportError:
-            # If it fails, try to easy install it
-            easy_install.main(["--user", package_name])
-            print "done."
-
+    import pip
 except ImportError:
-    print "Couldn't locate 'easy_install'. Do you have setuptools installed on your machine? Try sudo apt-get install python-setuptools (Linux) or use Homebrew (Mac)."
+    print "setup_LAMA requires 'pip' to be installed on your system\n"
+
+dependencies = {
+    'yaml': 'pyyaml',
+    'scipy': 'scipy',
+    'numpy': 'numpy',
+    'SimpleITK': 'SimpleITK',
+    'appdirs': 'appdirs',
+    'psutil': 'psutil',
+    'yaml': 'pyyaml'
+
+}
+
+failed_installs = []
+
+for import_name, package_name in dependencies.iteritems():
+
+    try:
+        print "Installing {0}...".format(import_name),
+        mod = __import__(import_name)  # try to import module
+        print " already installed.".format(import_name)
+
+    except ImportError:
+        # If it fails, try to easy install it
+        result = pip.main(["--user", package_name])
+        if result != 0:
+            failed_installs.append(package_name)
+
+if len(failed_installs) == 0:
+    print "All packages successfully installed"
+else:
+    print 'The following packages failed to install\n'
+    for failed in failed_installs:
+        print "{}\n".format(failed)
+
+if 'SimpleITK' in failed_installs:
+
+    itk_url = 'http://sourceforge.net/projects/simpleitk/files/SimpleITK/0.9.0/Python/SimpleITK-0.9.0-py2.7-linux-x86_64.egg'
+    print "\nSometimes SimpleITK can be problematic to install\n"
+    print "Download the puthon egg from here\n\t{}\n\n".format(itk_url)
+    print "And install with this command\n"
+    print "easy_install --user SimpleITK-0.9.0-py2.7-linux-x86_64.egg"
+
 
