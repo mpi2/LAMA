271c190632c3d65cec9c82176f640d325ece0178
Neil horner <n.horner@har.mrc.ac.uk>
2016-09-15 15:17:16 +0100

    Added utility script
    Former-commit-id: 426ac5bb417ea0c694e530ddd7d29488fd4b6c82 [formerly b49cf3acef864851e25c640db016bb535ff72ce1]
    Former-commit-id: 81d1324c9463c05bb99bd5130d830dbe01473cc4

---
+++ utilities/residuals.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env/python
+
+import os
+import SimpleITK as sitk
+import numpy as np
+from matplotlib import pyplot as plt
+from tempfile import TemporaryFile
+
+EXTENSION = ('nrrd', 'tif')
+
+
+def run(wt_dir, mut_dir, out_dir):
+
+
+    wt_paths = [os.path.join(wt_dir, x) for x in os.listdir(wt_dir)]
+    mut_paths = [os.path.join(mut_dir, x) for x in os.listdir(mut_dir)]
+    paths = wt_paths + mut_paths
+
+    memmap_arrays = memmap_data(paths)
+
+    mean = np.mean(memmap_arrays, axis=0)
+    for i, array in enumerate(memmap_arrays):
+        errors = mean - np.array(array)
+        bins = np.linspace(-1, 1, num=2048)
+        hist1, bins = np.histogram(errors, bins=bins)
+        width = 1 * (bins[1] - bins[0])
+        center = (bins[:-1] + bins[1:]) / 2
+        plt.bar(center, hist1, width=width, color='blue', align='center', alpha=0.4, linewidth=0)
+        title = os.path.splitext(os.path.basename(paths[i]))[0]
+        plt.title(title)
+        plt.savefig(os.path.join(out_dir, title + '.png'))
+        plt.close()
+
+
+def memmap_data(paths):
+    mm_arrays = []
+    for f in paths:
+        img = sitk.ReadImage(f)
+        arr = sitk.GetArrayFromImage(img)
+        t = TemporaryFile()
+        mm = np.memmap(t, dtype=arr.dtype, mode='w+', shape=arr.shape)
+        mm[:] = arr
+        mm_arrays.append(mm)
+    return mm
+
+
+if __name__ == '__main__':
+    import sys
+    wt_folder = sys.argv[1]
+    mut_folder = sys.argv[2]
+    out_dir = sys.argv[3]
+    run(wt_folder, mut_folder, out_dir)
