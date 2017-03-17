081d47c141021e34712e003380d8ad4bb2a45186
Neil horner <n.horner@har.mrc.ac.uk>
2017-03-17 16:16:52 +0000

    OrganVolumeStats now using linear model
    Signed-off-by: Neil horner <n.horner@har.mrc.ac.uk>
    
    Former-commit-id: 44743ee1db36be63e9428fa086eb1f0a8dd4677c [formerly 71b40eddbd9069d602677f75139a736a66ac7046]
    Former-commit-id: 1c57d4bbc58806a19d96a11dcc90a708c1823079

---
+++ skeleton_length.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+
+
+from scipy.ndimage.measurements import center_of_mass
+import os
+import SimpleITK as sitk
+import numpy as np
+from scipy.spatial.distance import cdist
+
+
+def run(in_dir):
+    for path, subdirs, files in os.walk(in_dir):
+        for name in files:
+            if not name.endswith('nrrd'):
+                continue
+            im_path = os.path.join(path, name)
+            img = sitk.ReadImage(im_path)
+            arr = sitk.GetArrayFromImage(img)
+            dist = skeletonize(arr)
+            print("{},{}".format(name, dist))
+
+
+def skeletonize(arr):
+    out_arr = np.zeros_like(arr)
+    points = []
+    for z, slice_ in enumerate(arr):
+        if np.any(slice_):
+            center = center_of_mass(slice_)
+            out_arr[z, int(center[0]), int(center[1])] = 1
+            points.append((z, center[0], center[1]))
+    # created two padded arrays so they are shifted relative to each other
+    points_1 = points[:]
+    points_1.insert(9, points[0])
+    points_2 = points[:]
+    points_2.append(points[-1])
+    # Pairs that we want are on the diagonal
+    distances = cdist(points_1, points_2)
+    adjacent_distances = np.diag(distances)
+    total_distance = np.sum(adjacent_distances)
+    return total_distance
+
+
+if __name__ == '__main__':
+
+    import argparse
+    parser = argparse.ArgumentParser("?")
+    parser.add_argument('-i', '--indir', dest='indir', help='Input path.', required=True)
+    args = parser.parse_args()
+
+    run(args.indir)
