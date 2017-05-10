b2f965f6d122e150c4e3d73da8f939e144f2112c
Neil horner <n.horner@har.mrc.ac.uk>
2017-05-10 14:36:12 +0100

    Just changing a command line option
    Former-commit-id: 87bd16c7a856f9c5812a24ce2850042896884047 [formerly 88956b0b9fb74469f5273b8c916868425f71cf79]
    Former-commit-id: 02273b855b494a2468f6fa6009607409e304a6be

--- utilities/imgstats.py
+++ utilities/imgstats.py
@@ -56,7 +56,7 @@ if __name__ == '__main__':
 
     import argparse
     parser = argparse.ArgumentParser("Get stats on images in a fodler")
-    parser.add_argument('-d', '--dir', dest='dir', help='directory with images', required=True)
+    parser.add_argument('-i', '--indir', dest='dir', help='directory with images', required=True)
     parser.add_argument('-o', '--out_file', dest='out_file', help='Optional path to csv file', default=False)
     args = parser.parse_args()
     img_stats(args.dir, args.out_file)
