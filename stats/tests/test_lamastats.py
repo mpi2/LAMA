de76fdbe4ea31d37db264efdde90560f7c7689f1
Neil horner <n.horner@har.mrc.ac.uk>
2017-08-31 13:35:36 +0100

    Fixed stats bug
    Former-commit-id: 971572ab0d5ac21410073e9adfa5126b13e63de7

--- stats/tests/test_lamastats.py
+++ stats/tests/test_lamastats.py
@@ -4,41 +4,41 @@ from staging.get_volumes_by_stage import VolumeGetter
 from tempfile import NamedTemporaryFile
 
 """
-nose tests for bu_lama_stats.py
-bu_lama_stats.py is the main script for running the statistical analysis for LAMA
+nose tests for lama_stats.py
+These tests just test the whole pipeline with different config file options
 """
-
-
 
 wt_staging_file = NamedTemporaryFile()
 mut_staging_file = NamedTemporaryFile()
 
 
 def setup():
-    print 'setting up'
-    with open(wt_staging_file.name, 'w') as fh:
-        fh.write(wt_data)
+	print
+	'setting up'
+	with open(wt_staging_file.name, 'w') as fh:
+		fh.write(wt_data)
+
 
 def save_mutant_file(data):
-    with open(mut_staging_file.name, 'w') as mf:
-        mf.write(data)
+	with open(mut_staging_file.name, 'w') as mf:
+		mf.write(data)
 
 
 @with_setup(setup)
 def test_get_config():
-    """
-    Test the function that gets all the
-    Returns
-    -------
+	"""
+	Test the function that gets all the
+	Returns
+	-------
 
-    """
-    mut_data = """vol,value
+	"""
+	mut_data = """vol,value
 mut1,3.0
 mut2,5.0
 mut3,10.0
 """
-    save_mutant_file(mut_data)
-    stager = VolumeGetter(wt_staging_file.name, mut_staging_file.name)
-    files = stager.filtered_wt_ids()
-    assert files == ['c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']
-    #stager.plot() # Coud write plot file
+	save_mutant_file(mut_data)
+	stager = VolumeGetter(wt_staging_file.name, mut_staging_file.name)
+	files = stager.filtered_wt_ids()
+	assert files == ['c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']
+	# stager.plot() # Coud write plot file
