02338adab13e3c13b399c734329e97051e2b85cb
Neil horner <n.horner@har.mrc.ac.uk>
2017-10-09 17:03:32 +0100

    Refactoring lama_stats
    Former-commit-id: 1768c2161515ee420bd4b1fc17ea3419b0e5ebb9

--- stats/stats_config_validation.py
+++ stats/stats_config_validation.py
@@ -1,5 +1,52 @@
+import yaml
+import logging
+import sys
+from os.path import join, dirname
+import difflib
+
+sys.path.insert(0, join(dirname(__file__), '..'))
+from lib.addict import Dict
+
+KNOWN_OPTIONS = (
+	'data', 'fixed_mask', 'label_map_path', 'littermate_controls', 'mut_staging_file', 'wt_staging_file', 'n1',
+	'voxel_size', 'invert_config_file', 'project_name', 'organ_names', 'blur_fwhm', 'formulas', 'Use_auto_staging'
+)
 
 
+def validate(config_path):
+	"""
+	Get the config and check for paths
+	"""
+	try:
+		with open(config_path) as fh:
+			try:
+				config = yaml.load(fh)
+			except Exception as e:  # Couldn't catch scanner error from Yaml
+				logging.error('Error reading stats yaml file\n\n{}'.format(e))
+				sys.exit()
+	except IOError:
+		logging.error("cannot find or open stats config file: {}".format(config_path))
+		sys.exit(1)
+	addict_config = Dict(config)
 
-def validate_stats_config(config_path):
-    pass
+	incorrect = unkown_options(config)
+	if incorrect:
+		sys.exit('incorrect option "{}" in stats yaml file.\nDo you mean {} ?'.format(*incorrect))
+
+	try:
+		config['data']
+	except KeyError:
+		logging.error("stats config file needs a 'data' entry. Are you usin gthe correct config file?")
+
+	else:
+		# Check each data entry for correct options (not done yet
+		pass
+	return addict_config
+
+
+def unkown_options(config):
+	for param in config:
+		if param not in KNOWN_OPTIONS:
+			closest_match = difflib.get_close_matches(param, KNOWN_OPTIONS)
+			return param, closest_match
+	return False
