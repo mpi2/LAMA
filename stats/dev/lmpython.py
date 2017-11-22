bc7aab6d60ea227d2d87de34a655236a7e6f3b86
n.horner <n.horner@har.mrc.ac.uk>
2017-11-22 21:40:38 +0000

    Switched off n=1 for organvols stats
    Former-commit-id: f2c479fdefaea115ee761381c1af64b872b355b6

--- stats/dev/lmpython.py
+++ stats/dev/lmpython.py
@@ -8,19 +8,25 @@ Try to implement the LM function in numpy in order to speed things up
 import numpy as np
 from scipy.stats import t as t_
 
+sample_n = 10000
+
 from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import OneHotEncoder
 n = 8
-y = np.array([0, 1,2,3, 5,6,7,8])
-y = y.reshape((8,1))
+# y = np.array([0, 1,2,3, 5,6,7,8])
+# y = y.reshape((8,1))
 x = np.array([0,0,0,0,2,2,2,2])
+# enc = OneHotEncoder()
 x = x.reshape((8,1))
 
-y = np.column_stack((y, y))
+y = np.random.random(sample_n * 8).reshape((8, sample_n))
+
+# y = np.column_stack((y, y))
 
 # y[0][0] = 10
 
 model = LinearRegression(fit_intercept=True, copy_X=True)
-fit= model.fit(x, y)
+fit = model.fit(x, y)
 pred = fit.predict(x)
 
 # Test until I do maean on column
@@ -32,7 +38,7 @@ coef = fit.coef_.flatten()
 print('coef', coef)
 t = coef/se_slope
 print('t', t)
-p = t_.sf(t, n-2)*2 # *2 for two sided test
+p = t_.sf(t, n-2)*2  # *2 for two sided test
 print('p', p)
 
 
