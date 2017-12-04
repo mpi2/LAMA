5d396d7a73d4050ab13f2c4197696da7ecdbd46e
n.horner <n.horner@har.mrc.ac.uk>
2017-12-04 13:48:21 +0000

    Added some tests for the lama stats
    Former-commit-id: 35f5edaa6667a55cd6c618d1e92218a518638976 [formerly 68997e3d0fda06bfb0f0a570145f9c7725a9dd13]
    Former-commit-id: ebba8a66ca8b868df67412f76a3c0a48330161fa

--- stats/tests/test_run_lama_stats.py
+++ stats/tests/test_run_lama_stats.py
@@ -1,4 +1,5 @@
 import nose
+from nose.tools import nottest, raises
 import os
 from os.path import join, realpath, dirname, isdir
 import sys
@@ -10,6 +11,9 @@ import run_lama_stats
 Run all the config files in the test config directory 
 Currently just running and making sure there's no uncaught exceptions.
 TODO: check that the correct output is generated too
+
+To run these tests, the test data needs to be fected from bit/dev/lama_stats_test_data
+In future we should put this in a web-accessible place
 """
 
 
@@ -17,6 +21,18 @@ current_dir = dirname(realpath(__file__))
 
 CONFIG_DIR = join(current_dir, 'test_data', 'config_files')
 
+
 def test_all():
 	config = join(CONFIG_DIR, 'all_specimens.yaml')
+	run_lama_stats.run(config)
+
+
+def test_all():
+	config = join(CONFIG_DIR, 'all_specimens.yaml')
+	run_lama_stats.run(config)
+
+
+@raises(IOError)
+def test_missing_config():
+	config = join(CONFIG_DIR, 'all_specimens.flaml')
 	run_lama_stats.run(config)
