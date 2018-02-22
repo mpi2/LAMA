Fixed bug in normalization
Former-commit-id: 77ca8625c252f06178f276438b017c8bd5b246ea [formerly 6f2dd5d15eaad8ce3c9f6b03c5de54dfd42609fb]
Former-commit-id: f3e803711520b292809eaf0b3c92949e9676974a

qc/roi_overlay.py
@@ -78,7 +78,7 @@ def make_normalization_roi_qc_images(img_dir, roi, out_dir):
             else:
                 images.append(rgb_arr)
 
-        # Vreate an image array the combined width of the threee images
+        # create an image array the combined width of the threee images
         max_height = max(heights)
         total_width = sum(widths)
         out_img_arr = np.zeros(max_height * total_width * 3).reshape((max_height, total_width, 3))