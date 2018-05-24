86c75306dbbb671cdf389eb293be1698b2f4bad5
neil <n.horner@har.mrc.ac.uk>
2018-05-24 21:45:05 +0100

    added option to not clobber when inverting registrations"
    Former-commit-id: edbc601241fa7ab47cbad2b17dcebae26ea49df2 [formerly 3704177fb3e35deee8973c4fbdc3ce56a8fd2d61]
    Former-commit-id: b0a8c45f6a777a2ab16af2156066e288bd5e4387

--- setup_LAMA.py
+++ setup_LAMA.py
@@ -4,10 +4,15 @@ print "=== LAMA phenotyping pipeline ===\nDownloading dependencies..."
 
 
 # easy_install first
+import sys
+import urllib
+import tempfile
+from os.path import join
 try:
     import pip
 except ImportError:
-    print "setup_LAMA requires 'pip' to be installed on your system\n"
+    print "setup_LAMA requires 'pip' to be installed on your system\non ubuntu try 'sudo apt install python-pip'"
+    sys.exit()
 
 dependencies = {
     'scipy': 'scipy',
@@ -21,7 +26,8 @@ dependencies = {
     'pandas': 'pandas',
     'seaborn': 'seaborn',
     'pandas': 'pandas',
-    'statsmodels': 'statsmodels'
+    'statsmodels': 'statsmodels',
+    'PIL': 'Pillow'
 
 }
 
@@ -40,6 +46,23 @@ for import_name, package_name in dependencies.iteritems():
         if result != 0:
             failed_installs.append(package_name)
 
+#### Download and install pyradiomics package
+temp_dir = tempfile.gettempdir()
+zip_loc = join(temp_dir, "pyradiomics_master.zip")
+
+pyrad_url = 'https://github.com/Radiomics/pyradiomics/archive/master.zip'
+pyrad_file = urllib.URLopener()
+pyrad_file.retrieve(pyrad_url, "pyradiomics_master.zip")
+
+import zipfile
+zip_ref = zipfile.ZipFile(path_to_zip_file, 'r')
+zip_ref.extractall(directory_to_extract_to)
+zip_ref.close()
+
+
+python -m pip install -r requirements.txt
+python setup.py install
+
 if len(failed_installs) == 0:
     print "All packages successfully installed"
 else:
@@ -47,12 +70,5 @@ else:
     for failed in failed_installs:
         print "{}\n".format(failed)
 
-if 'SimpleITK' in failed_installs:
-
-    itk_url = 'http://sourceforge.net/projects/simpleitk/files/SimpleITK/0.9.0/Python/SimpleITK-0.9.0-py2.7-linux-x86_64.egg'
-    print "\nSometimes SimpleITK can be problematic to install\n"
-    print "Download the python egg from here\n\t{}\n\n".format(itk_url)
-    print "And install with this command\n"
-    print "easy_install --user SimpleITK-0.9.0-py2.7-linux-x86_64.egg"
 
 
