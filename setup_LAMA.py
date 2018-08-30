643efbfc9c1e04a65643b2983f9204b62dd40103
Neil Horner <horner.neil@gmail.com>
2018-08-30 14:16:05 +0100

    Fixed some issues with the invert lock
    Former-commit-id: 25693dcc53865865934e6de2894b2dc3f14dedd9

--- setup_LAMA.py
+++ setup_LAMA.py
@@ -3,6 +3,10 @@
 import subprocess as sub
 import os
 
+
+# This fixes the problem of pipenv breaking pip
+# export PATH="${HOME}/.local/bin:$PATH"
+
 # How to ensure we use python3.6?
 
 VENV_NAME = 'lama-venv'
@@ -16,30 +20,37 @@ venv_path = os.path.join(script_dir,  VENV_NAME)
 
 print('installing python virtual environment')
 
+# res = sub.check_output(['pip3', 'install', '--user', 'pipenv'])
+# print(res)
+
 try:
-    res = sub.check_output(['python3', '-m', 'venv', venv_path])
-except sub.CalledProcessError as e: # Migt
+    res = sub.check_output(['pipenv', '--python', '3.6'])
+except sub.CalledProcessError as e:
     print(e.output)
     raise SystemExit
-python_bin = os.path.join(script_dir, VENV_NAME, 'bin', 'python3')
-pip3_bin = os.path.join(script_dir, VENV_NAME, 'bin', 'pip3')
 
-req = os.path.join(script_dir, 'requirements.txt')
 
-print('installing python packages')
-sub.check_output([python_bin, pip3_bin, 'install', '-r', req])
+res = sub.check_output(['pipenv', 'install'])
 
-site_packages = os.path.join(script_dir, VENV_NAME, 'lib', 'python3.6', 'site-packages')
+# python_bin = os.path.join(script_dir, VENV_NAME, 'bin', 'python3')
+# pip3_bin = os.path.join(script_dir, VENV_NAME, 'bin', 'pip3')
 
-pth_file_path = os.path.join(site_packages, 'lama.pth')
+# req = os.path.join(script_dir, 'requirements.txt')
 
-dirs_to_add_to_path = ['lib', 'elastix', 'img_processing']
+# print('installing python packages')
+# sub.check_output([python_bin, pip3_bin, 'install', '-r', req])
 
-print('Setting up paths')
-with open(pth_file_path, 'w') as fh:
-    for dir_ in dirs_to_add_to_path:
-        path_to_add = os.path.join(script_dir, dir_)
-        fh.write("{}\n".format(path_to_add))
+# site_packages = os.path.join(script_dir, VENV_NAME, 'lib', 'python3.6', 'site-packages')
+#
+# pth_file_path = os.path.join(site_packages, 'lama.pth')
+
+# dirs_to_add_to_path = ['lib', 'elastix', 'img_processing']
+
+# print('Setting up paths')
+# with open(pth_file_path, 'w') as fh:
+#     for dir_ in dirs_to_add_to_path:
+#         path_to_add = os.path.join(script_dir, dir_)
+#         fh.write("{}\n".format(path_to_add))
 
 print('Succesfully setup virtual envoronment\n'
       "run 'source lama-venv/bin/activate'\n"
