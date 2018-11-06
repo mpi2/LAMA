e07e4d976c5171c164ba5789d0f8bbb156f8634d
Neil Horner <horner.neil@gmail.com>
2018-11-06 15:39:24 +0000

    Upated some rscripts
    Former-commit-id: 113cdb5d3e106dbdf4665d566418353eeec55e1f [formerly 8dbb758f5233a11e63f5edc254b0de2a45c831de]
    Former-commit-id: 0c5eac5ed54888090e5256ac0b248f03263172ed

--- stats/rscripts/plots_from_models.R
+++ stats/rscripts/plots_from_models.R
@@ -10,7 +10,8 @@ df_whole <- read.csv('/home/neil/IMPC_research/neil/E14.5/baselines/staging_info
 
 df_organs <- read.csv('/home/neil/IMPC_research/neil/E14.5/baselines/raw_wt_organ_vols.csv')
 
-out_dir <- '/home/neil/IMPC_research/neil/E14.5/baselines/QC/regression_plots'
+out_dir <- '/home/neil/IMPC_research/neil/E14.5/baselines/QC/regression_plots_311018'
+dir.create(out_dir, showWarnings = FALSE)
 
 individ_label_dir = file.path(out_dir, 'per_label')
 dir.create(individ_label_dir, showWarnings = FALSE)
@@ -26,16 +27,16 @@ colnames(df_whole)[1] <- 'vol'
 colnames(df_whole)[2] <- 'whole'
 colnames(df_crl)[2] <- 'crl'
 
-bics_volume <- vector()
-bics_boxcox <- vector()
-bics_volume_crl <- vector()
-bics_boxcox_crl <- vector()
+
 
 
 r2_whole <- vector()
+r2_whole_log <- vector()
 r2_whole_box_cox <- vector()
+r2_whole_log_cox <- vector()
 r2_crl <- vector()
 r2_crl_boxcox <- vector()
+labels <- vector()
 
 df_test = merge(x=df_crl, y=df_whole, by.x='vol', by.y='vol')
 df_test1 = merge(x=df_test, y=df_organs, by.x='vol', by.y='vol')
@@ -62,16 +63,18 @@ for(label in names(df_test1)) {
   if (all(df_test1[[label]] == 0)){
     next()
   }
-  #
-  # if (label != 'X187'){
-  #   next()
-  # }
-  
+
   label_dir <- file.path(individ_label_dir, label)
   dir.create(label_dir, showWarnings = FALSE)
   
   print(label)
+  labels <- append(labels, label)
+
+
+
+  ################################## whole embryo########################################
   fit_whole <- lm(df_test1[[label]] ~ df_test1$whole)
+  fit_whole_log <- lm(log(df_test1[[label]]) ~ log(df_test1$whole))
   
 
   Box <- boxcox(df_test1[[label]] ~ df_test1$whole, plotit = FALSE, lambda = seq(-2, 2, len = 4000))
@@ -80,53 +83,61 @@ for(label in names(df_test1)) {
   lambda = Cox2[1, "Box.x"]
   tformed <- bcPower(df_test1[[label]], lambda)
 
+  Box <- boxcox(log(df_test1[[label]]) ~ log(df_test1$whole), plotit = FALSE, lambda = seq(-2, 2, len = 4000))
+  Cox = data.frame(Box$x, Box$y)
+  Cox2 = Cox[with(Cox, order(-Cox$Box.y)),]
+  lambda = Cox2[1, "Box.x"]
+  tformed_log <- bcPower(df_test1[[label]], lambda)
+
   fit_whole_box <-  lm(tformed~  df_test1$whole)
+  fit_whole_log_box <-  lm(tformed_log~  df_test1$whole)
   
   r2_whole <- append(r2_whole, summary(fit_whole)$r.squared)
+  r2_whole_log <- append(r2_whole_log, summary(fit_whole_log)$r.squared)
   r2_whole_box_cox <- append(r2_whole_box_cox, summary(fit_whole_box)$r.squared)
+  r2_whole_log_cox <- append(r2_whole_log_cox, summary(fit_whole_log_box)$r.squared)
 
-  bics_volume <- append(bics_volume, BIC(fit_whole))
-  bics_boxcox <- append(bics_boxcox, BIC(fit_whole_box))
-
-
-  ################################## whole embryo
-  ###### no normalisation
 
   ## Save qq plots
   title_ = paste(label, 'whole embryo')
-  plot_path = file.path(label_dir, paste(label, '.png'))
+  plot_path = file.path(label_dir, paste(label, 'whole.png'))
   plot_lm(fit_whole, title_, plot_path)
 
-  #####  boxcox transformed
-  ## Save qq plots
-  # devAskNewPage(ask = FALSE)
   title_ = paste(label, 'whole embryo boxcox')
-  plot_path = file.path(label_dir, paste(label, 'boxcox.png'))
+  plot_path = file.path(label_dir, paste(label, 'whole_boxcox.png'))
   plot_lm(fit_whole_box, title_, plot_path)
+
+  title_ = paste(label, 'whole embryo log_boxcox')
+  plot_path = file.path(label_dir, paste(label, 'whole_boxcox_log.png'))
+  plot_lm(fit_whole_log_box, title_, plot_path)
+
+  title_ = paste(label, 'whole embryo log')
+  plot_path = file.path(label_dir, paste(label, 'whole_log.png'))
+  plot_lm(fit_whole_log, title_, plot_path)
+
+
+
 
   ############## Using CRl ################################
   
   fit_crl <- lm(log(df_test1[[label]]) ~ df_test1$crl)
-  
+
   Box <- boxcox(df_test1[[label]] ~ df_test1$crl, plotit = FALSE, lambda = seq(-2, 2, len = 4000))
   Cox <- data.frame(Box$x, Box$y)
   Cox2 <- Cox[with(Cox, order(-Cox$Box.y)),]
   lambda <- Cox2[1, "Box.x"]
   tformed <- bcPower(df_test1[[label]], lambda)
-  
+
   fit_crl_box <-  lm(tformed~  df_test1$crl)
 
   r2_crl <- append(r2_crl, summary(fit_crl)$r.squared)
   r2_crl_boxcox <- append(r2_crl_boxcox, summary(fit_crl_box)$r.squared)
-  
-  bics_volume_crl <- append(bics_volume_crl, BIC(fit_crl))
-  bics_boxcox_crl <- append(bics_boxcox_crl, BIC(fit_crl_box))
 
   ## Save qq plots
   plot_path = file.path(label_dir, paste(label, 'crl.png'))
   title_ <- paste(label, 'CRL')
   plot_lm(fit_crl, title_, plot_path)
-  
+
   #####  boxcox transformed
   ## Save qq plots
   plot_path = file.path(label_dir, paste(label, 'boxcox_crl.png'))
@@ -137,27 +148,14 @@ for(label in names(df_test1)) {
 }
 
 
-
-## Save bics plots etc
-bics_df <- data.frame(bics_volume, bics_boxcox, bics_volume_crl, bics_boxcox_crl)
-colnames(bics_df) <- c('whole_embryo', 'whole_embryo_bc', 'crl', 'crl_bc')
-
-write.csv(bics_df, file.path(out_dir, 'bics.csv'))
-
-boxplot_path = file.path(out_dir, 'bics.png')
-boxplot(bics_df)
-png(file=boxplot_path)
-dev.off()
-
-
 ## save r2 plots and csv
-r2_df <- data.frame(r2_whole, r2_whole_box_cox, r2_crl, r2_crl_boxcox)
-colnames(bics_df) <- c('whole_embryo', 'whole_embryo_bc', 'crl', 'crl_bc')
+r2_df <- data.frame(labels, r2_whole, r2_whole_box_cox, r2_whole_log, r2_whole_log_cox, r2_crl, r2_crl_boxcox)
+colnames(r2_df) <- c('label', 'whole_embryo', 'whole_embryo_bc', 'whole_embryo_log', 'whole_embryo_log_box', 'crl', 'crl_bc')
 
 write.csv(r2_df, file.path(out_dir, 'r2.csv'))
 
 boxplot_path = file.path(out_dir, 'r2.png')
-boxplot(bics_df)
+boxplot(r2_df)
 png(file=boxplot_path)
 dev.off()
 
