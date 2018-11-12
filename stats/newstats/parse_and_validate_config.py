d88e570e796a6ee6c58d5fbb49447701908e0f43
neil <n.horner@har.mrc.ac.uk>
2018-11-12 16:24:25 -0500

    Started working on stats rewrite
    Former-commit-id: 3e98450e1fa03e0292f09bfb548dafe2175f5f48 [formerly 2140bf1aca935f4c32aff34c21fa966904894215]
    Former-commit-id: 6d3a23b5eb6a99a2fb4ac1894d08b327fca3b48c

---
+++ stats/newstats/parse_and_validate_config.py
@@ -0,0 +1,11 @@
+from pathlib import Path
+from typing import List
+
+class LamaStatsData(object):
+    pass
+
+def get_stats_objects(config_path: Path) -> List[LamaStatsData]:
+    validate_config()
+
+def validate_config():
+    pass
