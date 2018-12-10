43c7de9de41bd6e7e0d038132c1d18fd8b43f651
neil <n.horner@har.mrc.ac.uk>
2018-12-10 17:26:52 -0500

    Added scripts for getting test and tutorial data
    Former-commit-id: 7c40a9fc8c4a5a70091848db6835530bd40d7ba5 [formerly 36070a81458001df443c17bee8fd9c22b239d100]
    Former-commit-id: 7cf4199fe529e8a4baf311fa0c4647c23d5e271c

---
+++ scripts/get_tutorial_data.py
@@ -0,0 +1,10 @@
+from pathlib import Path
+from lama.common import download_and_extract_zip
+
+url = 'https://www.mousephenotype.org/images/lama/tutorial_data.zip'
+test_dir = Path(__file__).parent.parent / 'tests'
+
+download_and_extract_zip(url, test_dir)
+
+
+
