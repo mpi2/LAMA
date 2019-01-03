d36999f47599a521963d946ba7b847d991b42f24
Neil Horner <horner.neil@gmail.com>
2019-01-03 16:27:22 +0000

    removed junk folder
    Former-commit-id: 3df1b3852336ccaf64820daa94c1bde70089d31a

--- lama/junk/staging.py
+++
@@ -1,7 +0,0 @@
-"""
-A nose test file to test the ability of the staging in lama_stats.py
-"""
-
-
-# Lets assume we have a range of mutant label segmentations. Could be crown to rump lengths or whole embryo volumes
-mut_staging_file = 'staging_examples/mut_label_staging.csv'
