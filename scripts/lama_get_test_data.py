8f383e85a2c3de2a8e79b40928d00140541b9bc8
Neil Horner <horner.neil@gmail.com>
2019-01-09 15:25:15 +0000

    Updated lama_env.sh file
    Former-commit-id: ee35a492d6c1e7bde04e48e6c0e140fab0e71f61

--- scripts/lama_get_test_data.py
+++ scripts/lama_get_test_data.py
@@ -1,5 +1,3 @@
-#! python3
-
 from pathlib import Path
 from lama.common import download_and_extract_zip
 
--- scripts/lama_reg.py
+++ scripts/lama_reg.py
@@ -3,12 +3,11 @@ Script to run the lama registration piepline
 """
 import argparse
 
-from lama.registration_pipeline.run_lama import RegistrationPipeline
+from lama.registration_pipeline import run_lama
 
 if __name__ == "__main__":
-
     parser = argparse.ArgumentParser("The LAMA registration pipeline")
     parser.add_argument('-c', dest='config', help='Config file (YAML format)', required=True)
     args = parser.parse_args()
 
-    RegistrationPipeline(args.config)
+    run_lama.run(args.config)
