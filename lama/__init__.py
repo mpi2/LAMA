dafe26cae23008b1cc172d99a49a94075ec8832c
neil <n.horner@har.mrc.ac.uk>
2019-01-26 23:12:56 -0500

    Fixed matplotlib problem when no X-server
    Former-commit-id: 22cd6189a0f62388622332d1ed77f5fc39e06839 [formerly fe81315d325dab66a45342c5124a472d246face2]
    Former-commit-id: ff95fca2a6525a29a987d42a8f19094211d00154

--- lama/__init__.py
+++ lama/__init__.py
@@ -1,4 +1,8 @@
 import warnings
+import matplotlib
 
 # When running from a docker image, get much warnings from sklearn, pandas etc. Turn it off
 warnings.filterwarnings("ignore")
+
+
+matplotlib.use('Agg')
