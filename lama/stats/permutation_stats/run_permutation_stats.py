568ccb7773f133f1375be5b319e856739eee2aca
Neil Horner <n.horner@har.mrc.ac.uk>
2019-03-12 16:59:13 +0000

    Couple of doc string edits before branching
    Former-commit-id: 4c3a102640d5431aff156e1616f3afec352abe5a [formerly 346e77bb6de15c41082b147db04914ba4ac09b47]
    Former-commit-id: ff9974c4a22385d67a5a40a395573429229551c6

--- lama/stats/permutation_stats/run_permutation_stats.py
+++ lama/stats/permutation_stats/run_permutation_stats.py
@@ -241,7 +241,8 @@ def prepare_data(wt_organ_vol: pd.DataFrame,
                  log_staging: bool = False,
                  log_dependent: bool = False) -> pd.DataFrame:
     """
-    Do some pre-processing on the input DataFrames and concatenate into one
+    Do some pre-processing on the input DataFrames and concatenate into one data frame.
+
 
     Returns
     -------
@@ -321,7 +322,7 @@ def run(wt_dir: Path, mut_dir: Path, out_dir: Path, num_perms: int, log_dependen
                         mut_staging,
                         log_dependent)
 
-    out_dir.mkdir(exist_ok=True, parents=True) # Root directory for output
+    out_dir.mkdir(exist_ok=True, parents=True)  # Root directory for output
 
     # make directory to store distributions and thresholds
     dists_out = out_dir / 'distributions'
@@ -365,23 +366,3 @@ def run(wt_dir: Path, mut_dir: Path, out_dir: Path, num_perms: int, log_dependen
 
     non_uniques.to_csv(dists_out / 'non_uniques.csv')
 
-
-if __name__ == '__main__':
-    import argparse
-
-    parser = argparse.ArgumentParser("Permutation-based stats")
-    parser.add_argument('-w', '--wt_dir', dest='wt_dir', help='wildtype registration directory',
-                        type=argparse.FileType('w'),
-                        required=True)
-    parser.add_argument('-m', '--mut_dir', dest='mut_dir', help='mutant registration directory',
-                        type=argparse.FileType('w'),
-                        required=True)
-    parser.add_argument('-o', '--out_dir', dest='out_dir', help='permutation results directory',
-                        type=argparse.FileType('w'),
-                        required=True)
-    parser.add_argument('-n', '--num_perm', dest='num_perm', help='number of permutations to do', type=np.int,
-                        required=False, default=1000)
-
-    args = parser.parse_args()
-
-    run(args.wt_dir, args.mut_dir, args.out_dir, args.num_perm)
