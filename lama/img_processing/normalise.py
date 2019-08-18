Fixed bug in qc metric plots. They were dispalyed in the wrong order
Former-commit-id: e2f659df22cda517deb4e5cc4524456f3ce34b03 [formerly bbff07297caa13007e6d437ae211fabb2fc8b2ca]
Former-commit-id: c7f3eb6c82f87bccf8f2e44906920caedae8377b

lama/img_processing/normalise.py
@@ -45,7 +45,7 @@ class Normaliser:
 
     def memorymap_data(self, lama_root_dir: Path) -> Dict[str, np.memmap]:
         """
-        Iterate over a alam output folder getting each ...........
+        Iterate over output folder getting each ...........
         Parameters
         ----------
         lama_root_dir