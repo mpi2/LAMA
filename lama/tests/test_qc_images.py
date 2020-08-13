605539841e167ed41e8930e62ed55a1eb3fb1f77
Neil Horner <horner.neil@gmail.com>
2020-08-13 13:13:34 +0100

    Switched off histogram matching in qc images as red cyan overlays were not working

---
+++ lama/tests/test_qc_images.py
@@ -0,0 +1,10 @@
+"""
+Functions to test QC image creation.
+Manual checking of the output is required to make sure the images are as expected.
+
+Note: These testes require test_data_generation.py to have been run previously
+"""
+
+import pytest
+
+from lama.qc import qc_images
