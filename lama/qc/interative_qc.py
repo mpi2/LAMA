312573881d5def8cfd4f31421647e017b5851ff3
Neil Horner <horner.neil@gmail.com>
2020-08-21 13:17:30 +0100

    updated qc script

--- lama/qc/interative_qc.py
+++ lama/qc/interative_qc.py
@@ -149,7 +149,7 @@ def do_qc(root: Path, csv_or_dir: Path):
             line_out_dir = csv_or_dir / spec.line_id
             line_out_dir.mkdir(exist_ok=True)
             spec_out_file = line_out_dir / f'{spec.specimen_id}.jpg'
-            plt.savefig(spec_out_file, quality=70, optimize=True, progressive=True)
+            plt.savefig(spec_out_file, quality=80, optimize=True, progressive=True)
 
 
 
