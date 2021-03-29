c639f2f777df28ddcfa46492cb1d7aae7741df56
Neil Horner <horner.neil@gmail.com>
2021-03-29 21:00:05 +0100

    Updated tests

--- lama/tests/run_tests.sh
+++ lama/tests/run_tests.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
- script_dir=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
- pytest -m "not notest" $script_dir
+
+script_dir=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+
+pytest -m "not notest" "$script_dir"/test_data_generation.py; # Run this first as the stats tests need the output
+pytest -m "not notest" "$script_dir"/test_standard_stats.py;
+pytest -m "not notest" "$script_dir"/test_permutation_stats.py; # Run this first as the stats tests need the output
