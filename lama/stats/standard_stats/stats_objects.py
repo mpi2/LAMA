2a995feb2875d348bdad3f6c4cc41cf4f2a3c9de
Kyle Drover <41981766+Dorky-Lever@users.noreply.github.com>
2021-04-13 18:55:40 +1000

    update for two_way stats

--- lama/stats/standard_stats/stats_objects.py
+++ lama/stats/standard_stats/stats_objects.py
@@ -31,7 +31,8 @@ class Stats:
     def __init__(self,
                  input_: LineData,
                  stats_type: str,
-                 use_staging: bool = True
+                 use_staging: bool = True,
+                 two_way: bool = False
                  ):
         """
 
@@ -48,6 +49,7 @@ class Stats:
         self.stats_type_ = stats_type
         self.stats_runner = None
         self.use_staging = use_staging
+        self.two_way = two_way
 
         # The final results will be stored in these attributes
         self.line_qvals = None
@@ -66,7 +68,9 @@ class Stats:
 
     def run_stats(self):
 
-        if self.use_staging:
+        if self.two_way:
+            logging.info('Using genotype, treatment and staging in linear model')
+        elif self.use_staging:
             logging.info('Using genotype and staging in linear model')
         else:
             logging.info('Using only genotype in linear model')
@@ -90,31 +94,50 @@ class Stats:
 
             current_chunk_size = data_chunk.shape[1]  # Final chunk may not be same size
 
-            p_all, t_all = self.stats_runner(data_chunk, info, use_staging=self.use_staging)
+            p_all, t_all = self.stats_runner(data_chunk, info, use_staging=self.use_staging, two_way=self.two_way)
 
+                       
             # Convert all NANs in the pvalues to 1.0. Need to check that this is appropriate
             p_all[np.isnan(p_all)] = 1.0
 
             # Convert NANs to 0. We get NAN when for eg. all input values are 0
             t_all[np.isnan(t_all)] = 0.0
 
+            # chunk size isn't a thing in two_ways from what I can tell
+            if self.two_way:
+                #just append the line vals into the array
+                for index, pval in enumerate(p_all):
+                    line_level_pvals.append(pval[:current_chunk_size])
+                    line_level_tvals.append(t_all[index][:current_chunk_size])
+                
             # Each chunk of results has the line -level results at the start
-            p_line = p_all[:current_chunk_size]
-            t_line = t_all[:current_chunk_size]
-
-            line_level_pvals.append(p_line)
-            line_level_tvals.append(t_line)
-
+            else:
+                p_line = p_all[:current_chunk_size]
+                t_line = t_all[:current_chunk_size]
+                line_level_pvals.append(p_line)
+                line_level_tvals.append(t_line)
+                     
             # Get the specimen-level statistics
             mut_ids = self.input_.mutant_ids()
 
             for spec_num, id_ in enumerate(mut_ids):
-                # After the line level result, the specimen-level results are appended to the result chunk
                 start = current_chunk_size * (spec_num + 1)
                 end = current_chunk_size * (spec_num + 2)
 
-                specimen_tstats[id_].append(t_all[start:end])
-                specimen_pvals[id_].append(p_all[start:end])
+                if self.two_way: 
+                    
+                    for index, pval in enumerate(p_all):
+                        
+                        t_stat = t_all[index]
+                        
+                        specimen_tstats[id_].append(t_stat[start:end]) #just add ll pvals to the row
+                        specimen_pvals[id_].append(pval[start:end])
+                    
+
+                else: 
+                
+                    specimen_tstats[id_].append(t_all[start:end])
+                    specimen_pvals[id_].append(p_all[start:end])
 
         # Stack the results chunks column-wise to get back to orginal shape
         line_pvals_array = np.hstack(line_level_pvals)
