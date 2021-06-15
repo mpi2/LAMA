a9800dc42fcf459e0b614d8fda4d72837279368e
Neil Horner <horner.neil@gmail.com>
2021-06-15 13:24:15 +0100

    version update

--- lama/version.py
+++ lama/version.py
@@ -1,3 +1,4 @@
-__version__ = '0.9.82' 
+__version__ = '0.9.83' # Fixed a a couple of minor stats bugs
+#__version__ = '0.9.82'
 #__version__ = '0.9.81' # Fixed stats InvertHeatemap bug
 #__version__ = '0.9.80'
