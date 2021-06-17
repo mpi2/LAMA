ea03248bbe9edc613c5e55c97c2c592ea6a8fdfb
Neil Horner <horner.neil@gmail.com>
2021-06-17 15:48:36 +0100

    Version update

--- lama/version.py
+++ lama/version.py
@@ -1,4 +1,5 @@
-__version__ = '0.9.83' # Fixed a a couple of minor stats bugs
+__version__ = '0.9.90' # Permutation stats now create null distributon per label. As QC can lead to difference data structure per label
+#__version__ = '0.9.83' # Fixed a a couple of minor stats bugs
 #__version__ = '0.9.82'
 #__version__ = '0.9.81' # Fixed stats InvertHeatemap bug
 #__version__ = '0.9.80'
