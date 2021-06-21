fc636511e5b2b74ae391c65978b6a4fccab55abd
Neil Horner <horner.neil@gmail.com>
2021-06-21 11:24:41 +0100

    Organ vol plots paths bug

--- lama/stats/permutation_stats/run_permutation_stats.py
+++ lama/stats/permutation_stats/run_permutation_stats.py
@@ -573,7 +573,8 @@ def run(wt_dir: Path,
         for col in data_for_plots.columns:
             if col.isdigit():
                 data_for_plots[col] = data_for_plots[col] * data_for_plots['staging']
-    make_plots(mut_dir, data_for_plots, label_info, lines_root_dir, voxel_size=voxel_size)
+
+    make_plots(data_for_plots, label_info, lines_root_dir, voxel_size=voxel_size)
 
     # Get specimen info. Currently just the WEV z-score to highlight specimens that are too small/large
     spec_info_file = out_dir / 'specimen_info.csv'
