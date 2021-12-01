82ffa5c3e7ebe04d3316af6af3090884157dea3e
Kyle Drover <41981766+dorkylever@users.noreply.github.com>
2021-12-01 12:22:19 +1100

    removed np.shape from one-way stats
    Yeah this makes sense I hope

--- lama/stats/standard_stats/results_writer.py
+++ lama/stats/standard_stats/results_writer.py
@@ -104,10 +104,16 @@ class ResultsWriter:
             spec_t = spec_res['t']
             spec_q = spec_res['q']
             spec_p = spec_res['p']
-            if np.shape(spec_t) > 0: 
+            if self.two_way:
+                if np.any(np.shape(spec_t)) > 0:
+                    write_threshold_file(spec_q, spec_t, spec_threshold_file)
+                    self._write(spec_t, spec_p, spec_q, specimen_out_dir, spec_id)
+
+            else:
                 write_threshold_file(spec_q, spec_t, spec_threshold_file)
                 self._write(spec_t, spec_p, spec_q, specimen_out_dir, spec_id)
-
+            
+            
         # self.log(self.out_dir, 'Organ_volume stats', results.input_)
         logging.info('Finished writing specimen-level results')
 
